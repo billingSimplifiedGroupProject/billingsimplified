72ba5441fc0ceb6f1bdb897436c4219c325b640e
Corey <coreybrobison@gmail.com>
2016-05-30 12:35:17 -0600

    Frontend connections

---
+++ Public/JS/app.js
@@ -0,0 +1,30 @@
+angular.module("app", ['ui.router'])
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('/login');
+        $stateProvider
+            .state('login', {
+                url: "/login",
+                templateUrl: 'Views/login.html',
+                controller: 'loginCtrl'
+            })
+            .state('practiceStaff', {
+                url: "/practiceStaff",
+                templateUrl: 'Views/practiceStaff.html',
+                controller: 'practiceStaffCtrl'
+            })
+            .state('practiceAdmin', {
+                url: "/practiceAdmin",
+                templateUrl: 'Views/practiceAdmin.html',
+                controller: 'practiceAdminCtrl'
+            })
+            .state('billingStaff', {
+                url: "/billingStaff",
+                templateUrl: 'Views/billingStaff.html',
+                controller: 'billingStaffCtrl'
+            })
+            .state('billingAdmin', {
+                url: "/billingAdmin",
+                templateUrl: 'Views/billingAdmin.html',
+                controller: 'billingAdminCtrl'
+            });
+    });
---
+++ Public/JS/controllers/practiceAdmin.controller.js
@@ -0,0 +1,5 @@
+angular.module("app").controller('practiceAdminCtrl', function($scope, practiceAdminService) {
+
+
+
+});
