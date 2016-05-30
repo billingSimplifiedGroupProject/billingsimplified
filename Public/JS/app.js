ce6a36f6a14f30fdf2f4ad8c03e3ec661f628a13
markstewart <markstewy@gmail.com>
2016-05-30 16:42:02 -0600

    changed file names to exclude a . and got static dir working on backend, should host with no errors on nodemon except for css errors due to git ignore for compling reasons

--- Public/JS/app.js
+++ Public/JS/app.js
@@ -1,37 +1,9 @@
-<<<<<<< HEAD
-angular.module('billingsimplified', ["ui.router"]).config(function($stateProvider, $urlRouterProvider){
-  $stateProvider
-  .state('login', {
-    url: '/login',
-    templateUrl: './Views/login.html',
-    controller: './contollers/login.contoller.js'
-  })
-  .state('adminBilling', {
-    url:'/adminBilling',
-    templateUrl: "./Views/billingAdmin.html"
-  })
-  .state('staffBilling', {
-    url: "/staffBilling",
-    templateUrl:"/Views/billingStaff.html"
-  })
-  .state('practiceAdmin', {
-    url:"/practiceAdmin",
-    templateUrl: "/Views/practiceAdmin.html"
-  })
-  .state('practiceStaff', {
-    url: "/practiceStaff",
-    templateUrl: "/Views/practiceStaff.html"
-  })
-
-})
-=======
 angular.module("app", ['ui.router'])
     .config(function ($stateProvider, $urlRouterProvider) {
-        $urlRouterProvider.otherwise('/login');
         $stateProvider
             .state('login', {
                 url: "/login",
-                templateUrl: 'Views/login.html',
+                templateUrl: './Views/login.html',
                 controller: 'loginCtrl'
             })
             .state('practiceStaff', {
@@ -53,6 +25,6 @@ angular.module("app", ['ui.router'])
                 url: "/billingAdmin",
                 templateUrl: 'Views/billingAdmin.html',
                 controller: 'billingAdminCtrl'
-            });
+            })
+            $urlRouterProvider.otherwise('./login');
     });
->>>>>>> master
---
+++ public/JS/services/loginSvc.js
@@ -0,0 +1,10 @@
+angular.module("app").service("loginSvc", function() {
+
+   // $scope.login = function() {
+   //    mainSvc.login().then(function(res) {
+   //       $scope.logindata = res;
+   //       console.log(res);
+   //    })
+   // }
+
+});
