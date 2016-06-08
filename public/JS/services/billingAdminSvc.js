f5e34b67f1cf3d8d4c173d61b8ad25cfcc98550d
Kyle Barlow <kbazlo@yahoo.com>
2016-06-08 12:17:12 -0600

    added functinality to practice admin, cleaning up code and added small ui features like toaster

--- public/JS/services/billingAdminSvc.js
+++ public/JS/services/billingAdminSvc.js
@@ -1,17 +1,14 @@
 angular.module("app").service("billingAdminService", function($http) {
+
     this.createNewUser = function(data, practiceId) {
+        if (practiceId) {
+            data.practiceId = practiceId;
+        }
         console.log("service", data, practiceId);
         return $http({
             method: 'POST',
             url: '/create/user',
-            data: {
-                firstName: data.firstName,
-                lastName: data.lastName,
-                email: data.email,
-                password: data.password,
-                userType: data.userType,
-                practiceId: practiceId
-            }
+            data: data
         }).then(function(response) {
             console.log("service response", response);
             return response;
@@ -40,6 +37,17 @@ angular.module("app").service("billingAdminService", function($http) {
       return response;
     })
   };
+
+  this.deletePractice = function(id) {
+      $http({
+          method: "DELETE",
+          url: '/delete/practice/' + id
+      })
+        .then(function(response) {
+            console.log(response.data);
+            return response
+        })
+  }
 
   this.submitChart = function(practiceChart, id){
     practiceChart.practiceId = id;
