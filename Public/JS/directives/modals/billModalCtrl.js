76692d150098d770279469102b02d572e19d5915
Corey <coreybrobison@gmail.com>
2016-06-15 12:46:04 -0600

    finished functionality for summing payments and showing balanc due

--- Public/JS/directives/modals/billModalCtrl.js
+++ Public/JS/directives/modals/billModalCtrl.js
@@ -7,9 +7,30 @@ angular.module("app")
             if ($scope.billModalShown === false) {
                 billService.getBill(billId).then(function (response) {
                     console.log(response);
+
                     $scope.billInfo = response.data;
+
+                    $scope.total = response.data.billAmount;
+
+                    console.log($scope.total, "TOTAL THIS")
+
                     $scope.billInfo.dateDue = response.data.dateDue;
+
                     console.log($scope.billInfo);
+
+                    var paymentsFor = $scope.billInfo.payments
+
+                    $scope.calcTotalPaid = function (paymentsFor) {
+                        var totalPaid = 0;
+                        for (var i = 0; i < paymentsFor.length; i++) {
+                            totalPaid += paymentsFor[i].paymentAmount;
+                        } return totalPaid;
+                    }
+
+                    console.log($scope.calcTotalPaid($scope.billInfo.payments))
+
+                    $scope.balanceDue = $scope.total - $scope.calcTotalPaid($scope.billInfo.payments);
+
                     var today = Date.now();
                     var dueDate = new Date($scope.billInfo.dateDue);
                     console.log(dueDate);
@@ -21,16 +42,22 @@ angular.module("app")
 
                     $scope.daysPastDue = Math.floor(days);
 
+                    return $scope.billInfo;
 
-
-                    console.log(today);
-                    console.log(dueDate);
                 })
                 $scope.billModalShown = !$scope.billModalShown
             } else {
                 $scope.billModalShown = !$scope.billModalShown
             }
         };
+
+
+
+        // $scope.totalPaid = function () {
+        //     return $scope.payments.reduce(function (current, payment) { return current + payments.length; }, 0);
+        // };
+
+        console.log($scope.billInfo)
 
         $scope.makePayment = function (paymentInfo, billId) {
             console.log("hitting here")
